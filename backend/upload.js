f36194ed8cc5f6a9c8e4eb1827dc159fe3848f65
Varun-MSI <tvarun2014@gmail.com>
2024-07-08 00:10:28 +0530

    First

--- backend/upload.js
+++ backend/upload.js
@@ -1,6 +1,9 @@
 const multer = require('multer');
 const inMemoryStorage = multer.memoryStorage();
 
-const upload = multer({ storage: inMemoryStorage });
+const upload = multer({ 
+    storage: inMemoryStorage,
+    limits: { fileSize: 100 * 1024 * 1024 } 
+  });
 
 module.exports = upload;
