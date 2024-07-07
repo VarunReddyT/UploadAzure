19a77df8c62e6dfc24e984d9c96cbf060d277e4f
Varun-MSI <tvarun2014@gmail.com>
2024-07-07 23:51:39 +0530

    First

--- src/Components/Upload.jsx
+++ src/Components/Upload.jsx
@@ -9,7 +9,7 @@ export default function Upload() {
     formData.append('video', file)
 
     try {
-      const res = await axios.post('http://localhost:4000/upload', formData, {
+      const res = await axios.post('https://upload-azure-three.vercel.app/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
