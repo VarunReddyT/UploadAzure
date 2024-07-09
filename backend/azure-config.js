b5b0cd95081dce4cedcc7475a1024a0a70d96233
Varun-MSI <tvarun2014@gmail.com>
2024-07-09 14:33:59 +0530

    New

--- backend/azure-config.js
+++ backend/azure-config.js
@@ -1,5 +1,6 @@
 const { BlobServiceClient } = require('@azure/storage-blob');
 
-const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+// const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+const blobServiceClient = BlobServiceClient.fromConnectionString("DefaultEndpointsProtocol=https;AccountName=tfdvideostry;AccountKey=orB6L7JgevGPglh3XFLXyflTJlnhXj6ZI/+jfeKGMCZZiWiQE1L92/bcCSnkCyMuDO92sqJ+SKUJ+ASt1jVJUw==;EndpointSuffix=core.windows.net");
 
 module.exports = { blobServiceClient };
--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,9 @@ const { v4: uuidv4 } = require('uuid');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const formidable = require('formidable');
+const upload = require('./upload.js');
 dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,35 +15,29 @@ app.use(cors({ origin: 'https://uploadazure.vercel.app' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.post('/upload', (req, res) => {
-  const form = new formidable.IncomingForm();
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-
-    const file = files.video;
-    const containerName = process.env.AZURE_STORAGE_CONTAINER_NAME;
+app.post('/upload', upload.single('video'), async (req, res) => {
+  try {
+    const containerName = "videos";
     const containerClient = blobServiceClient.getContainerClient(containerName);
-    const blobName = uuidv4() + '-' + file.originalFilename;
+    const blobName = uuidv4() + '-' + req.file.originalname;
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-    try {
-      await blockBlobClient.uploadStream(file.stream, file.size, 5, {
-        blobHTTPHeaders: { blobContentType: file.mimetype }
-      });
+    await blockBlobClient.uploadData(req.file.buffer, {
+      blobHTTPHeaders: { blobContentType: req.file.mimetype }
+    });
 
-      const videoUrl = blockBlobClient.url;
-      console.log(videoUrl);
-      res.status(200).json({ message: 'Video uploaded successfully', url: videoUrl });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  });
+    const videoUrl = blockBlobClient.url;
+    res.status(200).json({ message: 'Video uploaded successfully', url: videoUrl });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
