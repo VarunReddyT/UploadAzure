8fd722cf6e478be805342f56dff7911e29c645cb
Varun-MSI <tvarun2014@gmail.com>
2024-07-09 14:38:15 +0530

    New

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({ origin: 'https://uploadazure.vercel.app' }));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
